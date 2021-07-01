3d768a019ef100223b52759f70d3186e06eea0a3
sanjibsinha <sanjib12sinha@gmail.com>
2021-07-01 17:26:11 +0530

    modifying parent child relationship page

--- wp-content/themes/codetwo/page.php
+++ wp-content/themes/codetwo/page.php
@@ -18,17 +18,69 @@ while (have_posts()) {
 
   <div class="container container--narrow page-section">
 
+  <?php 
+
+  $theParentID = wp_get_post_parent_id(get_the_ID());
+  
+  if ($theParentID) { ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentID); ?>">
+      <i class="fa fa-home" aria-hidden="true">
+      </i> Back to <?php echo get_the_title($theParentID); ?></a> <span class="metabox__main">
+      <?php the_title( ) ?>
+      </span></p>
     </div>
+  
+  <?php }
+  
+  ?>    
+
+  <?php 
+
+  $theChildOfParent = get_pages(array('child_of' => get_the_ID(  ))); 
+
+  if($theParentID or $theChildOfParent) { ?>
     
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="
+      <?php
+      
+      echo the_permalink( $theParentID );
+      
+      ?>
+      ">
+      <?php 
+      
+      echo get_the_title( $theParentID );
+      
+      
+      ?>
+      
+      </a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php
+
+        if($theParentID) {
+          $theChildPages = $theParentID;
+        } else {
+          $theChildPages = get_the_ID(  );
+        }
+
+
+        $listOfChildPages = array(
+          'title_li' => NULL,
+          'child_of' => $theChildPages,
+          'sort_column' => 'menu_order',
+        );
+        
+        wp_list_pages( $listOfChildPages);
+        
+        
+        ?>
       </ul>
-    </div> -->
+    </div>
+
+    <?php } ?>
 
     <div class="generic-content">
       <?php the_content(  ) ?>
